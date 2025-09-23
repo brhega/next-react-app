7f4bc022a208353f76b5c91aa699cb308b01f17f
Brock Hegarty <brockhegarty@msn.com>
2025-09-23 16:27:48 -0500

    nav bar setup

---
+++ components/nav-bar.tsx
@@ -0,0 +1,29 @@
+import {
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenu,
+    NavigationMenuList,
+    NavigationMenuContent
+} from "@/components/ui/navigation-menu";
+
+export default function NavBar() {
+    return (
+        <div className="border-b">
+            <div className="flex justify-center">
+                <NavigationMenu>
+                    <NavigationMenuList>
+                        <NavigationMenuItem>
+                            <NavigationMenuLink href="/" className="text-lg">Home</NavigationMenuLink>
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>
+                            <NavigationMenuLink href="/about" className="text-lg">About</NavigationMenuLink>
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>
+                            <NavigationMenuLink href="/contact" className="text-lg">Contact</NavigationMenuLink>
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                </NavigationMenu>
+            </div>
+        </div>
+    );
+}
