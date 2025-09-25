7c3561920c4472473f1a1d4c103041c59ae2ea8c
Brock Hegarty <brockhegarty@msn.com>
2025-09-25 10:12:03 -0500

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      Hello!
+    <div className="flex items-center justify-center flex-col min-h-screen gap-8">
+      <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">Next.js and React Sample App</h1>
+      <h3 className="scroll-m-20 text-xl font-semibold tracking-tight">This is a sample project site dedicated to learning Next.js and React.  
+        It's focused around best practices, key concepts, and tools to know while building with a modern JS framework.
+      </h3>
     </div>
   );
 }
