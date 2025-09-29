0b41790aaf5c1ae1090058f38425a7cda26bcd96
Brock Hegarty <brockhegarty@msn.com>
2025-09-29 17:48:06 -0500

    link

--- app/examples/search-page/ProductCard.tsx
+++ app/examples/search-page/ProductCard.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@/components/ui/button";
 import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Product } from "@/lib/api/api-client";
+import Link from "next/link";
 
 export default function ProductCard({ product }: { product: Product }) {
     return (
@@ -8,7 +10,11 @@ export default function ProductCard({ product }: { product: Product }) {
                 <CardHeader>
                     <CardTitle>{product.name}</CardTitle>
                     <CardDescription>{product.description}</CardDescription>
-                    <CardAction>View Product</CardAction>
+                    <CardAction>
+                        <Button asChild variant="link" size="sm">
+                            <Link href={`/products/${product.id}`}>View Product</Link>
+                        </Button>
+                    </CardAction>
                 </CardHeader>
                 <CardContent>
                     <p>Available: {product.numAvailable}</p>
